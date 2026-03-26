1790e1d164a6b444b4fb831ed6653c5ac2383264
SclRlLiX <georg.m985@gmail.com>
2026-03-26 20:10:04 +0100

    added export to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -14,10 +14,13 @@ const config = {
 		}
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html' // Helps with client-side routing
+		}),
+		paths: {
+			// Replace 'your-repo-name' with your actual GitHub repository name
+			base: process.argv.includes('dev') ? '' : '/timekit'
+		}
 	}
 };
 
