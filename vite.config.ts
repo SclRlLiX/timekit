e8e096949a932c0d89f21f9d2ea37efc27c72012
SclRlLiX <georg.m985@gmail.com>
2026-04-03 13:57:57 +0200

    updated Icons

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,12 @@ export default defineConfig({
 				display: 'standalone',
 				icons: [
 					{
-						src: 'timekit-icon-192.png',
+						src: 'icon-192.png',
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: 'timekit-icon-512.png',
+						src: 'icon-512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'any maskable'
