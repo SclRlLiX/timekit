22587cca9ec5d17dec2a3df4065e0a43558037e5
SclRlLiX <georg.m985@gmail.com>
2026-04-03 13:40:39 +0200

    added transition when changing sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import { activeSection } from '$lib/activeSection';
-    import WorkPage from '$lib/components/Pages/WorkPage.svelte'
-    import SleepPage from '$lib/components/Pages/SleepPage.svelte';
+	import WorkPage from '$lib/components/Pages/WorkPage.svelte';
+	import SleepPage from '$lib/components/Pages/SleepPage.svelte';
+	import { fade } from 'svelte/transition';
 </script>
 
-{#if $activeSection === 'work'}
-	<WorkPage/>
-{:else}
-	<SleepPage/>
-{/if}
+<div class="relative">
+  {#key $activeSection}
+    <div transition:fade={{ duration:200 }} class="absolute inset-0">
+      {#if $activeSection === 'work'}
+        <WorkPage />
+      {:else}
+        <SleepPage />
+      {/if}
+    </div>
+  {/key}
+</div>
