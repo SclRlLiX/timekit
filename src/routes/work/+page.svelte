ee4b5fe64b4575832dfc8f1e2e1e41cbed3e7637
SclRlLiX <georg.m985@gmail.com>
2026-04-04 12:49:02 +0200

    now /work and /sleep is accessable vir the URL again

---
+++ src/routes/work/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { activeSection } from '$lib/activeSection';
+    import { resolve } from '$app/paths'
+
+	onMount(() => {
+		activeSection.set('work');
+		goto(resolve("/"));
+	});
+</script>
